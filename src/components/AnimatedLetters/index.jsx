1c10362488410cbebe673a1c3d3ce0b13e8a4ee3
StevenMetz <stevenm.codes@gmail.com>
2023-05-01 10:26:16 -0500

    header name animates with hover effect

---
+++ src/components/AnimatedLetters/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import './index.scss'
+export function AnimatedLetters({ letterClass, strArray, idx }) {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,58 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { AnimatedLetters } from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 export function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['S', 't', 'e', 'v', 'e', 'n', ' ', 'M', 'e', 't', 'z']
+  const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'E',
+    'n',
+    'g',
+    'i',
+    'n',
+    'e',
+    'e',
+    'r',
+  ]
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm Steven Metz
+          <span className={`${letterClass} _11`}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m </span>{' '}
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={26}
+          />
         </h1>
         <br />
-        <h2> Full-Stack Engineer / Ruby on Rails / Javascript </h2>
+        <h2> Full-Stack / Ruby on Rails / Javascript </h2>
         <Link to="/contact" className="flat-button">
           CONTACT ME
         </Link>
