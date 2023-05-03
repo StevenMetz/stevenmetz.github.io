deb07d904318e3136147088876944709bc9cbfce
StevenMetz <stevenm.codes@gmail.com>
2023-05-02 22:45:51 -0500

    portfolio webpage boilerplate made

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,13 @@
+import Loader from 'react-loaders'
+import './index.scss'
+
+export function Portfolio() {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
